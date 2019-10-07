80017adc5e7a1324c64b0d98ccfda4ce0c7daad9
Zhou Shaowen <dcszhsha@nus.edu.sg>
2019-10-07 11:27:37 +0800

    autopep

--- GradientBoostingRegressor.py
+++ GradientBoostingRegressor.py
@@ -52,27 +52,30 @@ class MyGradientBoostingRegressor():
     def get_model_dict(self):
         model_dict = dict()
         for i in range(self.n_estimators):
-            model_dict.update({str(i):self.estimators[i].root})
+            model_dict.update({str(i): self.estimators[i].root})
         return model_dict
 
     def save_model_to_json(self, file_name):
         model_dict = dict()
         for i in range(self.n_estimators):
-            model_dict.update({str(i):self.estimators[i].root})
+            model_dict.update({str(i): self.estimators[i].root})
 
         with open(file_name, 'w') as fp:
             json.dump(model_dict, fp)
 
 
 # For test
-if __name__=='__main__':
+if __name__ == '__main__':
     for i in range(1):
-        x_train = np.genfromtxt("Test_data" + os.sep + "x_" + str(i) +".csv", delimiter=",")
-        y_train = np.genfromtxt("Test_data" + os.sep + "y_" + str(i) +".csv", delimiter=",")
+        x_train = np.genfromtxt("Test_data" + os.sep +
+                                "x_" + str(i) + ".csv", delimiter=",")
+        y_train = np.genfromtxt("Test_data" + os.sep +
+                                "y_" + str(i) + ".csv", delimiter=",")
 
         for j in range(2):
             n_estimators = 10 + j * 10
-            gbr = MyGradientBoostingRegressor(n_estimators=n_estimators, max_depth=5, min_samples_split=2)
+            gbr = MyGradientBoostingRegressor(
+                n_estimators=n_estimators, max_depth=5, min_samples_split=2)
             gbr.fit(x_train, y_train)
             model_dict = gbr.get_model_dict()
 
@@ -81,7 +84,8 @@ if __name__=='__main__':
             with open("Test_data" + os.sep + "gradient_boosting_" + str(i) + "_" + str(j) + ".json", 'r') as fp:
                 test_model_dict = json.load(fp)
 
-            y_test_pred = np.genfromtxt("Test_data" + os.sep + "y_pred_gradient_boosting_"  + str(i) + "_" + str(j) + ".csv", delimiter=",")
+            y_test_pred = np.genfromtxt(
+                "Test_data" + os.sep + "y_pred_gradient_boosting_" + str(i) + "_" + str(j) + ".csv", delimiter=",")
 
             if compare_json_dic(model_dict, test_model_dict) * compare_predict_output(y_pred, y_test_pred) == 1:
                 print("True")
