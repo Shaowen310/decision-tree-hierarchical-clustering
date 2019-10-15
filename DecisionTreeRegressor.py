3ca7bf55a5d53bab7365d2cf1b945f8984437e1b
Zhou Shaowen <dcszhsha@nus.edu.sg>
2019-10-15 15:20:36 +0800

    make MSE computation simple

--- DecisionTreeRegressor.py
+++ DecisionTreeRegressor.py
@@ -32,22 +32,17 @@ class MyDecisionTreeRegressor():
         compare_rhs = np.expand_dims(X_T, axis=2)
         tree_left = compare_lhs <= compare_rhs
         tree_right = compare_lhs > compare_rhs
-        y_tile = np.tile(y, X.size).reshape((X.shape[1], X.shape[0], -1))
         tree_left_count = np.sum(tree_left, axis=2)
         tree_right_count = np.sum(tree_right, axis=2)
-        tree_left_y = tree_left * y_tile
-        tree_left_y_sum = np.sum(tree_left_y, axis=2)
-        tree_right_y = tree_right * y_tile
-        tree_right_y_sum = np.sum(tree_right_y, axis=2)
-        c_hat_left = np.where(tree_left_count > 0,
-                              tree_left_y_sum / tree_left_count, 0)
-        c_hat_right = np.where(tree_right_count > 0,
-                               tree_right_y_sum / tree_right_count, 0)
 
-        mse = c_hat_left ** 2 * tree_left_count - \
-            np.sum(tree_left_y * np.expand_dims(c_hat_left, axis=2), axis=2) * 2 + \
-            c_hat_right ** 2 * tree_right_count - \
-            np.sum(tree_right_y * np.expand_dims(c_hat_right, axis=2), axis=2) * 2
+        y_tile = np.tile(y, X.size).reshape((X.shape[1], X.shape[0], -1))
+        tree_left_y = np.where(tree_left, y_tile, np.nan)
+        tree_right_y = np.where(tree_right, y_tile, np.nan)
+
+        # MSE is variance * length
+        mse = np.nanvar(tree_left_y, axis=2) * tree_left_count + \
+            np.where(tree_right_count,
+                     np.nanvar(tree_right_y, axis=2) * tree_right_count, 0)
 
         s_cand = np.where(np.abs(mse - np.min(mse)) < 1e-5, X_T, np.nan)
         split_0 = np.nonzero(~np.all(np.isnan(s_cand), axis=1))[0][0]
@@ -55,6 +50,9 @@ class MyDecisionTreeRegressor():
 
         j = split[0]
         s = X_T[split]
+
+        c_hat_left = np.nanmean(
+            np.where(tree_left, compare_lhs, np.nan), axis=2)
 
         if tree_left_count[split] == len(X):
             return c_hat_left[split]
